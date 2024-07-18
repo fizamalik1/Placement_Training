017e8953805f18dfbf7ddd74617ee97faf051e1d
Fiza <fizamalik0620@gmail.com>
2024-07-18 23:22:56 +0530

    "Test"

--- prog8.java
+++ prog8.java
@@ -1,6 +1,13 @@
+// Print numbers from 1 to 10 in straight as well as in reverse order
+
 public class prog8
 {
-    public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+    public static void main(String[] args)
+    {
+        for(int i=1,j=10 ; i<=10 && j>0 ; i++,j--)
+        {
+            System.out.println(i + " " + j);
+        }
+        
     }
 }
