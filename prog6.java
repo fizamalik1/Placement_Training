643baa06955c26fd67e3e2a2c91c8d0c054b3fe6
Fiza <fizamalik0620@gmail.com>
2024-07-18 23:14:08 +0530

    "Test"

--- prog6.java
+++ prog6.java
@@ -1,8 +1,10 @@
+// Division of 2 numbers using float datatype in result
+
 public class prog6 
 {
     public static void main(String[] args) 
     {
-        int a = 2;
+        int a = 20;
         int b = 3;
         float result = (float) a/b;
         System.out.println(result);
