e95e71da29e3aaded14ac7a410b298afd5b9243b
Fiza <fizamalik0620@gmail.com>
2024-07-18 23:02:43 +0530

    "Test"

--- prog1.java
+++ prog1.java
@@ -1,18 +1,20 @@
+// code using nested if-else for printing grades according to the marks as input
+
 public class prog1
 {
     public static void main(String args[]) 
     {
-      int marks = 98;
+      int marks = 50;
       
       if(marks > 90)
       {
           System.out.println("A");
       }
-      else if (marks < 80 && marks >= 90)
+      else if (marks > 80 && marks <= 90)
       {
           System.out.println("B");
       }
-      else if(marks < 70 && marks >= 80)
+      else if(marks > 70 && marks <= 80)
       {
           System.out.println("C");
       }
