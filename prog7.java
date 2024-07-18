a7be8faa3a04ff64b1eaa5cde02921175820584a
Fiza <fizamalik0620@gmail.com>
2024-07-18 23:16:08 +0530

    "Test"

--- prog7.java
+++ prog7.java
@@ -1,3 +1,6 @@
+// Print even numbers from 1 to 30 then odd number from 1 to 30 using 2 while loops
+// variables i & j , increment & decrement concept is used
+
 public class prog7 
 {
     public static void main(String[] args) 
