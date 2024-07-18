bd700c8773a4a43a8fba00bfb2eeccc16dadab4c
Fiza <fizamalik0620@gmail.com>
2024-07-18 23:12:57 +0530

    "Test"

--- prog5.java
+++ prog5.java
@@ -1,9 +1,12 @@
+// Division of 2 integer numbers 
+// If the result is not in integer then the output will only the number before decimal 
+// eg. 20/7 = 2 (no numbers after the decimal will appear as the datatype over here is INT)
 public class prog5
  {
     public static void main(String[] args) 
     {
         int x = 20;
-        int y = 5;
+        int y = 4;
         int result = x/y;
         System.out.println(result);
     }
