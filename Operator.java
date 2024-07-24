4f818423f09a116612276cdffdfbefcf475e9fd1
Fiza <fizamalik0620@gmail.com>
2024-07-24 22:34:16 +0530

    "Practice"

---
+++ Operator.java
@@ -0,0 +1,18 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a="+a);
+        int b = a++;
+        System.out.println("b="+b);
+        System.out.println("a="+a);
+        int c = ++a;
+        System.out.println("c="+c);
+        System.out.println("a="+a);
+        int d = a--;
+        System.out.println("d="+d);
+        System.out.println("a="+a);
+        int e = --a;
+        System.out.println("e="+e);
+        System.out.println("a="+a);
+    }
+}
