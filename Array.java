271358a881e8878e89688c65a0da7756707790e3
Fiza <fizamalik0620@gmail.com>
2024-07-29 23:12:37 +0530

    "Practice"

---
+++ Array.java
@@ -0,0 +1,24 @@
+public class Array {
+    public static void main(String[] args) {
+        //declaration
+        int[] A = new int[5];
+        //initialization
+        A[0]=1;
+        A[1]=2;
+        A[2]=3;
+        A[3]=4;
+        A[4]=5;
+        System.out.println("A[0] is \n" +A[0]);
+        System.out.println("A[1] is \n" +A[1]);
+        System.out.println("A[2] is \n" +A[2]);
+        System.out.println("A[3] is \n" +A[3]);
+        System.out.println("A[4] is \n" +A[4]);
+    }
+}
+
+/* Array is a linear data structure which stores multiple elements of same datatype.
+ * Index of array always start with 0 by default in Java & C both.
+ * A[0]=index & the array index ends at n-1.
+ * It contains initialization & declaration.
+ * It stores the elements in contiguous and adjacent memory locatios.
+ */
---
+++ Array1.java
@@ -0,0 +1,10 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5};
+        System.out.println("A[0] is \n" +A[0]);
+        System.out.println("A[1] is \n" +A[1]);
+        System.out.println("A[2] is \n" +A[2]);
+        System.out.println("A[3] is \n" +A[3]);
+        System.out.println("A[4] is \n" +A[4]);
+    }
+}
